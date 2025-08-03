e3b64fc45d51ae29424de8dccbc95cbf6660863e
Arkaj19 <arkajyotikarmakar56@gmail.com>
2025-08-04 01:02:53 +0530

    Added the constants.js file

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const LOGO_URL ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0vvulfbahjxjz6k4uwi";
+export const CDN_URL ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9698196&lng=77.7499721&restaurantId=";
