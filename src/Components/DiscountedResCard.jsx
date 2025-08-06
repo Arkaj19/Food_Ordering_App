83dd417518dac671375cd96426578e72b5dc5d26
Arkajyoti Karmakar <arkajyoti.karmakar@gyansys.com>
2025-08-06 17:24:06 +0530

    HOC V4 Applied the Discounts info to cards

--- src/Components/DiscountedResCard.jsx
+++ src/Components/DiscountedResCard.jsx
@@ -4,7 +4,7 @@ const DiscountedResCard = (RestaurantCard) => {
   return function wrapResCard({ discountInfo, ...props }) {
     return (
       <div className="relative">
-        <div>
+        <div className="absolute top-2 left-2 bg-orange-500 text-white px-3 py-1 rounded-r-lg shadow-md text-xs font-bold z-10">
           {discountInfo?.header} {discountInfo?.subHeader}
         </div>
         <RestaurantCard {...props} />
